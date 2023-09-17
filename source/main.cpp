405de184c5d08252a5b4e2346b2bb118f62cf848
Patrik Suhaj <suhajpato@gmail.com>
2023-09-17 23:49:46 +0200

    Added SDL nuget

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <SDL_main.h>
+#include <SDL.h>
 
 int main(int argc, char* argv[])
 {
+    SDL_LogMessage(0, SDL_LogPriority::SDL_LOG_PRIORITY_INFO, "hello world");
     return 0;
 }
